38314506505a9d2b7f252f05cefd08a3939e935a
Ankur Kunal <mrankur801@gmail.com>
2021-05-14 22:02:03 +0530

    Solved shuffle-the-array

---
+++ shuffle-the-array.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/shuffle-the-array/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    vector<int> shuffle(vector<int> &nums, int n)
+    {
+        vector<int> output;
+        for (int i = 0; i < n; i++)
+        {
+            output.push_back(nums[i]);
+            output.push_back(nums[i + n]);
+        }
+        return output;
+    }
+};
