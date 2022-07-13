a432cb3831c94b5717c81311f14f0ef78c900be2
KunalAnkur <mrankur801@gmail.com>
2022-07-13 18:39:44 +0530

    day 3

---
+++ LeetCode/Blind_75/Trees/Binary Tree Level Order Traversal/Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(root == NULL) return ans;
+        queue<TreeNode* > q;
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();
+            vector<int> temp;
+            for(int i = 0; i< size; i++){
+                TreeNode* curr = q.front();
+                q.pop();
+                temp.push_back(curr -> val);
+                if(curr -> left) q.push(curr -> left);
+                if(curr -> right) q.push(curr -> right);
+            }
+            
+            ans.push_back(temp);
+            
+        }
+        
+        return ans;
+    }
+};
---
+++ LeetCode/Blind_75/Trees/Binary Tree Level Order Traversal/readme.md
@@ -0,0 +1,4 @@
+## Binary Tree Level Order Traversal
+```
+use of queue and add null pointer after adding root in the q to determine about the completion of one level or we can create size variable inside the for loop and then iterate all over the queue until size get 0 the each time size get zero it show the completetion of one lever
+```
